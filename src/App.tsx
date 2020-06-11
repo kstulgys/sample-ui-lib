2e67caa6b0fae301856905ade391b29da7a68c7c
karolis <karolis.stulgys@gmail.com>
2020-06-11 18:19:46 +0300

    Add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
+/*@jsx jsx*/
+import { jsx, keyframes } from '@emotion/core'
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import { Box, Image, Text, Link, Stack } from '@chakra-ui/core'
+
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <Stack bg="gray.700" minH={['100vh']} px="4">
+      <Stack as="header" mx="auto" my="auto" alignItems="center" color="white">
+        <Image src={logo} alt="logo" size="64" animation={`${bounce} 1s ease infinite`} />
+        <Text>
+          Edit <Box as="code">src/App.tsx</Box> and save to reload.
+        </Text>
+        <Link isExternal href="https://reactjs.org">
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Stack>
+    </Stack>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
