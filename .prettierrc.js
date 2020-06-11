8c020dc85740d238cee53e4e038afc4691ca9435
karolis <karolis.stulgys@gmail.com>
2020-06-11 17:48:39 +0300

    Add eslint, prettier, pre-commit hook

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Change your rules accordingly to your coding style preferencies.
+  // https://prettier.io/docs/en/options.html
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 160,
+  tabWidth: 2,
+  useTabs: false,
+}
